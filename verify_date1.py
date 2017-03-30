f3bd1a2a775e127585a29949313336f338377246
Valeriy Tur <turlera@ukr.net>
2017-03-30 22:14:57 +0300

    output date

--- verify_date1.py
+++ verify_date1.py
@@ -1,16 +1,22 @@
 # Программа отбирающая файлы с датой позже чем заданная
 import os
+from datetime import datetime
 
 # Запрос исходных данных: путь к корневой папке и дата после которой
 # были созданы файлы
 path = input("Введите путь к иследуемой папке с файлами: ")
 date_file = input("Дата последних изменений файлов: ")
-# Получаем перечень всех файло и папок
+# Получаем перечень всех файлов и папок
 list_file = list(os.walk(path))
 list1 = []
-for item_generly in list_file:
-    for file_item in item_generly[2]:
-        list1.append(file_item)
+dir1 = []
+for rootdir, dirname, file_1 in list_file:
+
+    for file_item in file_1:
+        file_path = os.path.join(rootdir, file_item)
+        file_2 = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+        list1.append(file_2)
+
 print(list1)
 
 # Проходим по всем файлам каждой папки
